6c6aa3150faffcac798149e2db51cac57d0bdb8d
Daniel Hoogerwerf <45287356+danielhoogerwerf@users.noreply.github.com>
2020-01-25 15:44:00 +0100

    added comments

--- restart_audio.sh
+++ restart_audio.sh
@@ -1,1 +1,5 @@
+#!/bin/bash
+
+# Coreaudiod can sometimes hang when switching sound cards in Mojave/Catalina. 
+# This will kill it so that it restarts itself again and activates the correct sound card.
 sudo kill -9 `ps ax|grep 'coreaudio[a-z]' | awk '{print $1}'`
